02b116fb0f95d37c601523e918fc26deb28344d3
Susana FA <susana.fabant@educa.jcyl.es>
2020-11-20 09:40:22 +0100

    Añadida muestra de fichero de configuración de la base de datos con extensión MySQLi

--- mostrarcodigo/muestraConfiguracion.php
+++ mostrarcodigo/muestraConfiguracion.php
@@ -14,7 +14,7 @@
         <main>
             <section>
                 <header> 
-                    <h3>Archivo de constantes de configuración de base de datos con extensión PDO</h3>
+                    <h3>Archivo de constantes de configuración de base de datos</h3>
                 </header>
                 <article>
                     <header>
@@ -28,6 +28,20 @@
                          */
                          
                         highlight_file("../config/confDBPDO.php");
+                    ?>
+                </article>
+                <article>
+                    <header>
+                        <h4>Muestra código confDBMySQLi.php</h4>
+                    </header>
+                    <?php
+                        /**
+                         * @author Susana Fabián Antón
+                         * @since 11/11/2020
+                         * @version 11/11/2020
+                         */
+                         
+                        highlight_file("../config/confDBMySQLi.php");
                     ?>
                 </article>
             </section>
