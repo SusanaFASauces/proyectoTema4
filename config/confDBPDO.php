cbda6773a35718535d93903e389965b3b9d1943f
Susana FA <susana.fabant@educa.jcyl.es>
2020-11-20 09:40:36 +0100

    Añadida muestra de fichero de configuración de la base de datos con extensión MySQLi

--- config/confDBPDO.php
+++ config/confDBPDO.php
@@ -8,11 +8,11 @@
     //CONSTANTES PARA LA EXTENSIÓN PDO
 
     //constantes servidor casa
-    define('DSN', 'mysql:host=192.168.0.122;dbname=DAW208DBDepartamentos');
-    define('HOST', '192.168.0.122');
-    define('DBNAME', 'DAW208DBDepartamentos');
-    define('USER', 'usuarioDAW208DBDepartamentos');
-    define('PASSWORD', 'P@ssw0rd');
+//    define('DSN', 'mysql:host=192.168.0.122;dbname=DAW208DBDepartamentos');
+//    define('HOST', '192.168.0.122');
+//    define('DBNAME', 'DAW208DBDepartamentos');
+//    define('USER', 'usuarioDAW208DBDepartamentos');
+//    define('PASSWORD', 'P@ssw0rd');
      
     //constantes entorno desarrollo
 //    define('DSN', 'mysql:host=192.168.20.19;dbname=DAW208DBDepartamentos');
@@ -28,9 +28,9 @@
 //    define('PASSWORD', 'paso');
     
     //constantes entorno explotación 1and1
-//    define('DSN', 'mysql:host=db771560853.hosting-data.io;dbname=db771560853');
-//    define('HOST', 'db771560853.hosting-data.io');
-//    define('DBNAME', 'db771560853');
-//    define('USER', 'dbo771560853');
-//    define('PASSWORD', 'Covid1234$');
+    define('DSN', 'mysql:host=db771560853.hosting-data.io;dbname=db771560853');
+    define('HOST', 'db771560853.hosting-data.io');
+    define('DBNAME', 'db771560853');
+    define('USER', 'dbo771560853');
+    define('PASSWORD', 'Covid1234$');
 ?>
